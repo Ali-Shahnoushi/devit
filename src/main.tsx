fa1538d6e479a40db644ce1e7f83a8110720f783
Ali Shahnoushi <alishahnoushi@gmail.com>
2025-05-18 12:05:16 +0330

    config react hot Toast

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "@/App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import AOSInit from "./utils/aos";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
+    <AOSInit />
     <App />
-  </QueryClientProvider>
+    <Toaster
+      toastOptions={{
+        className: "!bg-base-200 !text-base-content",
+      }}
+    />
+  </QueryClientProvider>,
 );
