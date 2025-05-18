6a18dce867f0d17955b549ad1592b779a96ecb96
Ali Shahnoushi <alishahnoushi@gmail.com>
2025-05-18 11:39:32 +0330

    implement refresh token

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,72 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import "./index.css";
-import AppLayout from "./components/ui/AppLayout";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import { useEffect } from "react";
-import useThemeState from "./states/useThemeState";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import About from './pages/About'
+import Home from './pages/Home'
+import './index.css'
+import AppLayout from './components/ui/AppLayout'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import { useEffect } from 'react'
+import useThemeState from './states/useThemeState'
+import Profile from './pages/Profile'
+import ProfileLayout from './components/ui/ProfileLayout'
+import VerifiedEmail from './pages/VerifiedEmail'
+import { useRefreshToken } from './hooks/auth/useRefreshToken'
+import SendEmailVerification from './pages/SendEmailVerification'
+import ResetPassword from './pages/ResetPassword'
 
 function App() {
-  const { theme, toggleTheme } = useThemeState((state) => state);
+    const { theme, toggleTheme } = useThemeState((state) => state)
+    const { refreshUserToken } = useRefreshToken()
 
-  useEffect(() => {
-    document.querySelector("html")?.setAttribute("data-theme", theme);
-    if (theme === "dark") {
-      document.querySelector("html")?.classList.add("dark");
-    } else {
-      document.querySelector("html")?.classList.remove("dark");
-    }
-  }, [theme, toggleTheme]);
+    useEffect(() => {
+        document.querySelector('html')?.setAttribute('data-theme', theme)
+        if (theme === 'dark') {
+            document.querySelector('html')?.classList.add('dark')
+        } else {
+            document.querySelector('html')?.classList.remove('dark')
+        }
+    }, [theme, toggleTheme])
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>{" "}
-    </BrowserRouter>
-  );
+    useEffect(() => {
+        const interval = setInterval(
+            () => {
+                refreshUserToken()
+            },
+            60 * 55 * 1000
+        )
+
+        return () => clearInterval(interval)
+    }, [])
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<AppLayout />}>
+                    <Route path="/" element={<Home />} />
+                </Route>
+                <Route path="/about" element={<About />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route
+                    path="/verified/:userId/:token"
+                    element={<VerifiedEmail />}
+                />
+                <Route
+                    path="/email-verification"
+                    element={<SendEmailVerification />}
+                />
+                <Route path="/reset-password" element={<ResetPassword />} />
+
+                <Route path="/dashboard" element={<ProfileLayout />}>
+                    <Route index element={<p>dashboard</p>} />
+                    <Route path="settings" element={<p>settings</p>} />
+                    <Route path="posts" element={<p>posts</p>} />
+                    <Route path="profile" element={<Profile />} />
+                </Route>
+            </Routes>{' '}
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
