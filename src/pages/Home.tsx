5f1b3af2d08bcd47bf2a3662ebf1cd1e60d57960
Ali Shahnoushi <alishahnoushi@gmail.com>
2025-05-18 12:14:18 +0330

    start implement Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import Hero from "../components/ui/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+      <Hero />
+    </>
+  );
+}
