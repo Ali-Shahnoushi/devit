778d5d34bc4ede40d25f20b4595431d267426c7e
Ali Shahnoushi <alishahnoushi@gmail.com>
2025-05-18 11:59:41 +0330

    implement theme state hook

--- src/states/useThemeState.ts
+++ src/states/useThemeState.ts
@@ -1,27 +1,27 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-type TAvailableThemes = "light" | "dark";
+type TAvailableThemes = 'light' | 'dark'
 
 export interface IThemeState {
-  theme: TAvailableThemes;
+    theme: TAvailableThemes
 }
 
 export interface IThemeAction {
-  toggleTheme: () => void;
-  setTheme: (theme: IThemeState["theme"]) => void;
+    toggleTheme: () => void
+    setTheme: (theme: IThemeState['theme']) => void
 }
 
 const useThemeState = create<IThemeState & IThemeAction>()(
-  persist(
-    (set, get) => ({
-      theme: "dark",
-      toggleTheme: () =>
-        set({ theme: get().theme === "dark" ? "light" : "dark" }),
-      setTheme: (theme) => set({ theme }),
-    }),
-    { name: "theme" }
-  )
-);
+    persist(
+        (set, get) => ({
+            theme: 'dark',
+            toggleTheme: () =>
+                set({ theme: get().theme === 'dark' ? 'light' : 'dark' }),
+            setTheme: (theme) => set({ theme }),
+        }),
+        { name: 'theme' }
+    )
+)
 
-export default useThemeState;
+export default useThemeState
