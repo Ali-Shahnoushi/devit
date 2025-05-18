6dac74b3639ce4cb7163f95bf9acb5a916372d8e
Ali Shahnoushi <alishahnoushi@gmail.com>
2025-05-18 12:29:28 +0330

    implement get update Api hook

---
+++ src/hooks/auth/useGetUser.ts
@@ -0,0 +1,21 @@
+import { UserInfo } from '@/types'
+import { apiClient } from '@/utils'
+import { useQuery } from '@tanstack/react-query'
+
+export function useGetUser() {
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ['user'],
+        queryFn: async () => {
+            const response = await apiClient.get('auth/me/')
+
+            console.log('response data :', response.data.data)
+
+            return response.data.data as UserInfo
+        },
+        retry: false, // برای اینکه روی خطای 401 دوباره سعی نکنه
+    })
+
+    const isLoggedIn = !isError && !!data
+
+    return { isLoading, user: data ?? undefined, isLoggedIn }
+}
