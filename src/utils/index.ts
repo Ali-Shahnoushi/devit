9735acbdc9ea8a9f4eeca944fde45cb7a1e31c9b
Ali Shahnoushi <alishahnoushi@gmail.com>
2025-05-18 12:00:53 +0330

    config axios

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,16 @@
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import axios, { AxiosError } from "axios";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const apiClient = axios.create({
+  baseURL: "http://localhost:8000/api/",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "*/*",
+  },
+  withCredentials: true,
+});
